bfaab22929eeb2dddaec9201f8c62984cfb58e88
Rob Odil <rodil@godaddy.com>
2012-11-14 11:21:02 -0700

    Linted everything and got most everything working at a baseline level.

--- server.js
+++ server.js
@@ -1,166 +1,135 @@
-//setup Dependencies
-var connect = require('connect')
-    , express = require('express')
-    , port = (process.env.PORT || 8081);
+(function () {
+    "use strict";
 
-//Setup Express
-var server = express.createServer();
-server.configure(function(){
-    server.set('views', __dirname + '/views');
-    server.set('view options', { layout: false });
-    server.use(connect.bodyParser());
-    server.use(express.cookieParser());
-    server.use(express.session({ secret: "shhhhhhhhh!"}));
-    server.use(connect.static(__dirname + '/public'));
-    server.use(server.router);
-});
 
-//setup the errors
-server.error(function(err, req, res, next){
-    if (err instanceof NotFound) {
-        res.render('404.jade', { locals: { 
-                  title : '404 - Not Found'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX' 
-                },status: 404 });
-    } else {
-        res.render('500.jade', { locals: { 
-                  title : 'The Server Encountered an Error'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                 ,error: err 
-                },status: 500 });
+    function NotFound(msg) {
+        this.name = 'NotFound';
+        Error.call(this, msg);
+        Error.captureStackTrace(this, arguments.callee);
     }
-});
-server.listen( port);
 
-
-var postArray = [
-        {
-            id: 001,
-            title: 'Monkies in my code 1',
-            summary: 'summary: ',
-            details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
-            postdate: '2012-09-15 12:00:01',
-            author: 'rodil@'
-        },
-        {
-            id: 002,
-            title: 'Monkies in my code 2',
-            summary: 'summary: ',
-            details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
-            postdate: '2012-09-15 12:00:01',
-            author: 'rodil@'
-        },
-        {
-            id: 003,
-            title: 'Monkies in my code 3',
-            summary: 'summary: ',
-            details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
-            postdate: '2012-09-15 12:00:01',
-            author: 'rodil@'
-        },
-        {
-            id: 004,
-            title: 'Monkies in my code 4',
-            summary: 'summary: ',
-            details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
-            postdate: '2012-09-15 12:00:01',
-            author: 'rodil@'
-        },
-        {
-            id: 005,
-            title: 'Monkies in my code 5',
-            summary: 'summary: ',
-            details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
-            postdate: '2012-09-15 12:00:01',
-            author: 'rodil@'
-        },
-        {
-            id: 006,
-            title: 'Monkies in my code 6',
-            summary: 'summary: ',
-            details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
-            postdate: '2012-09-15 12:00:01',
-            author: 'rodil@'
-        },
-        {
-            id: 007,
-            title: 'Monkies in my code 7',
-            summary: 'summary: ',
-            details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
-            postdate: '2012-09-15 12:00:01',
-            author: null
-        }
-    ];
-
-
-///////////////////////////////////////////
-//              Routes                   //
-///////////////////////////////////////////
-
-/////// ADD ALL YOUR ROUTES HERE  /////////
-
-server.get('/', function(req,res){
-  res.render('index.jade', {
-    locals : { 
-              title : 'Your Page Title'
-             ,description: 'Your Page Description'
-             ,author: 'Your Name'
-             ,analyticssiteid: 'XXXXXXX' 
+    //setup Dependencies
+    var connect = require('connect'),
+        express = require('express'),
+        port = (process.env.PORT || 8081),
+        server = express.createServer(),
+        postArray = [
+            {
+                id: 1,
+                title: 'Monkies in my code 1',
+                summary: 'summary: ',
+                details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
+                postdate: '2012-09-15 12:00:01',
+                author: 'rodil@'
+            },
+            {
+                id: 2,
+                title: 'Monkies in my code 2',
+                summary: 'summary: ',
+                details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
+                postdate: '2012-09-15 12:00:01',
+                author: 'rodil@'
+            },
+            {
+                id: 3,
+                title: 'Monkies in my code 3',
+                summary: 'summary: ',
+                details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
+                postdate: '2012-09-15 12:00:01',
+                author: 'rodil@'
+            },
+            {
+                id: 4,
+                title: 'Monkies in my code 4',
+                summary: 'summary: ',
+                details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nunc sit amet sapien iaculis tincidunt bibendum commodo tellus. Proin sed egestas tellus. Cras ultricies elementum nisi, eu placerat lacus iaculis non. Nunc gravida congue tincidunt. Phasellus nec felis leo, vitae placerat lacus. Etiam luctus, ligula quis tempus accumsan, est dolor suscipit nulla, vitae viverra velit diam at turpis. Donec sed egestas dui. Donec consequat posuere dictum.',
+                postdate: '2012-09-15 12:00:01',
+                author: 'rodil@'
             }
-  });
-});
+        ];
 
-server.get('/unittest', function(req,res){
-  res.render('unittest.jade', {
-    locals : { 
-              title : 'Testing'
-            }
-  });
-});
 
-server.get('/post/:post_id', function(req, res) {
-    res.header('Content-Type', 'application/json');
-    var found = false;
-    for (var i = 0; i < postArray.length; i++) {
-        if (postArray[i].id == req.param('post_id')) {
-            res.send(JSON.stringify(postArray[i]));
-            found = true;
+    server.configure(function () {
+        server.set('views', __dirname + '/views');
+        server.set('view options', { layout: false });
+        server.use(connect.bodyParser());
+        server.use(express.cookieParser());
+        server.use(express.session({ secret: "shhhhhhhhh!"}));
+        server.use(connect.static(__dirname + '/public'));
+        server.use(server.router);
+    });
+
+    //setup the errors
+    server.error(function (err, req, res, next) {
+        if (err instanceof NotFound) {
+            res.render('404.jade', {locals: {title: '404 - Not Found', error: 'Not Found'}, status: 404});
+        } else {
+            res.render('500.jade', {locals: {title: 'The Server Encountered an Error', error: 'Server Error'}, status: 500});
         }
-    };
-    if (!found) {
-        res.send(JSON.stringify({err: 1}));
-    }
-});
-
-server.get('/posts', function(req, res) {
-    res.header('Content-Type', 'application/json');
-    res.send(JSON.stringify(postArray));
-});
+    });
+    server.listen(port);
 
 
 
 
-// ];
+    ///////////////////////////////////////////
+    //              Routes                   //
+    ///////////////////////////////////////////
 
-//A Route for Creating a 500 Error (Useful to keep around)
-server.get('/500', function(req, res){
-    throw new Error('This is a 500 Error');
-});
+    /////// ADD ALL YOUR ROUTES HERE  /////////
 
-//The 404 Route (ALWAYS Keep this as the last route)
-server.get('/*', function(req, res){
-    throw new NotFound;
-});
+    server.get('/', function (req, res) {
+        res.render('index.jade', {
+            locals: {
+                title: 'Your Page Title'
+            }
+        });
+    });
 
-function NotFound(msg){
-    this.name = 'NotFound';
-    Error.call(this, msg);
-    Error.captureStackTrace(this, arguments.callee);
-}
+    server.get('/unittest', function (req, res) {
+        res.render('unittest.jade', {
+            locals: {
+                title: 'Testing'
+            }
+        });
+    });
+
+    server.get('/post/:post_id', function (req, res) {
+        var found = false,
+            i = 0;
+        res.header('Content-Type', 'application/json');
+
+        for (i = 0; i < postArray.length; i += 1) {
+            if (postArray[i].id === req.param('post_id')) {
+                res.send(JSON.stringify(postArray[i]));
+                found = true;
+            }
+        }
+        if (!found) {
+            res.send(JSON.stringify({err: 1}));
+        }
+    });
+
+    server.get('/posts', function (req, res) {
+        res.header('Content-Type', 'application/json');
+        res.send(JSON.stringify(postArray));
+    });
 
 
-console.log('Listening on http://0.0.0.0:' + port );
+
+
+    // ];
+
+    //A Route for Creating a 500 Error (Useful to keep around)
+    server.get('/500', function (req, res) {
+        throw new Error('This is a 500 Error');
+    });
+
+    //The 404 Route (ALWAYS Keep this as the last route)
+    server.get('/*', function (req, res) {
+        throw new NotFound();
+    });
+
+
+    console.log('Listening on http://0.0.0.0:' + port);
+}());
--- test/qunitrunner.js
+++ test/qunitrunner.js
@@ -1,66 +1,69 @@
+/*global phantom, require, window, console, setInterval */
+
 var args = phantom.args;
 var page = require('webpage').create();
 
-page.open('file:///Users/rodil/projects/codecamp_2012_project/public/testpage.html', function (status) {
+page.open('./public/testpage.html', function (status) {
+    "use strict";
 
-  // Duel won't load from phantomjs... This adds a mock of duel that doesn't do anything. DON'T TEST DUEL ELEMENTS!!!!
-  page.evaluate(function () {
-    window.require = function () {
-      return {
-        then: function () {}
-      };
-    };
-  });
-  if (status !== 'success') {
-    console.error("Unable to access network");
-    phantom.exit(1);
-  } else {
-    page.evaluate(function () {
-        window.qunitStatus = {
-            done: false,
-            failed: false
-        };
-    });
-    var timeout = parseInt(args[1] || 60000, 10);
-    var start = Date.now();
-    var interval = setInterval(function() {
-      if (Date.now() > start + timeout) {
-        phantom.exit(124);
-      } else {
-        
-        var qunitStatus = page.evaluate(function() {
-          return window.qunitStatus;
+    if (status !== 'success') {
+        console.error("Unable to access network");
+        phantom.exit(1);
+    } else {
+        page.evaluate(function () {
+            window.qunitStatus = {
+                done: false,
+                failed: false
+            };
         });
+        var timeout = parseInt(args[1] || 60000, 10),
+            start = Date.now(),
+            interval = setInterval(function () {
+                var module = '',
+                    test = '',
+                    assertion = '',
+                    qunitStatus = page.evaluate(function () {
+                        return window.qunitStatus;
+                    });
 
-        if (qunitStatus.done) {
-          
-          
-          
-          if (qunitStatus.failed) {
-            console.log('FAILURE');
-            console.log('');
-            for(var module in qunitStatus.failedTests) {
-                console.log('  MODULE: ' + module);
-                for(var test in qunitStatus.failedTests[module]) {
-                    console.log('    TEST: ' + test);
-                    for(var assertion in qunitStatus.failedTests[module][test]) {
-                        console.log('      ASSERT: ' + qunitStatus.failedTests[module][test][assertion]);
+                if (Date.now() > start + timeout) {
+                    phantom.exit(124);
+                } else {
+                    if (qunitStatus.done) {
+                        if (qunitStatus.failed) {
+                            console.log('FAILURE');
+                            console.log('');
+                            for (module in qunitStatus.failedTests) {
+                                if (qunitStatus.failedTests.hasOwnProperty(module)) {
+                                    console.log('  MODULE: ' + module);
+                                    for (test in qunitStatus.failedTests[module]) {
+                                        if (qunitStatus.failedTests[module].hasOwnProperty(test)) {
+                                            console.log('    TEST: ' + test);
+                                            for (assertion in qunitStatus.failedTests[module][test]) {
+                                                if (qunitStatus.failedTests[module][test].hasOwnProperty(assertion)) {
+                                                    console.log('      ASSERT: ' + qunitStatus.failedTests[module][test][assertion]);
+                                                }
+                                            }
+                                        }
+                                    }
+                                    console.log('');
+                                }
+                            }
+                            console.log('STATUS: ' + qunitStatus.stats);
+                            console.log('');
+                            phantom.exit(1);
+                        } else {
+                            console.log('SUCCESS');
+                            console.log('');
+                            console.log(qunitStatus.stats);
+                            console.log('');
+                            phantom.exit();
+                        }
                     }
                 }
-                console.log('');
-            }
-            console.log('STATUS: ' + qunitStatus.stats);
-            console.log('');
-            phantom.exit(1);
-          } else {
-            console.log('SUCCESS');
-            console.log('');
-            console.log(qunitStatus.stats);
-            console.log('');
-            phantom.exit();
-          }
-        }
-      }
-    }, 500);
-  }
+            }, 500);
+    }
 });
+
+
+
